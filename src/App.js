67934c0e31c64d35b799180183d92e8c8bc5d96c
juca <juca@pop-os.localdomain>
2023-02-04 16:04:32 -0300

    att

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './feed/Footer';
 
 function App() {
   return(
-    <div className='min-h-screen  bg-body m-5 px-4 rounded-lg
+    <div className='min-h-screen overflow-x-hidden bg-body m-5 px-4 rounded-lg
      md:px-24 xl:px-24  2xl:px-48'>
       <Nav />
       <Main />
